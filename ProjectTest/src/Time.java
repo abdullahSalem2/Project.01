7d618f6890817ee4ece0c3ae86e87ac9b6cf579d
AG <AG@DESKTOP-CC70V6F>
2021-12-11 21:09:08 +0300

    Test

---
+++ ProjectTest/src/Time.java
@@ -0,0 +1,12 @@
+public class Time {
+
+	    int seconds;
+	    int minutes;
+	    int hours;
+
+	    public Time(int hours, int minutes, int seconds) {
+	        this.hours = hours;
+	        this.minutes = minutes;
+	        this.seconds = seconds;
+	    }
+}
